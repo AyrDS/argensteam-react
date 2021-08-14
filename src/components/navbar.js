166ea235bd65ab1b6b70d71c43499c4e08d523b2
AyrDS <ayrtonds97@gmail.com>
2021-08-14 19:00:54 -0300

    Desafio clase 3

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return(
+        <header id="header" className="header">
+            <h1>Tienda Virtual</h1>
+            <nav className="nav">
+                <a href="index.html">Link</a>
+                <a href="index.html">Link</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 1) Tener la variable React en scope
+import React from "react";
+// 2) Tener la variable ReactDOM en scope
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 3) Tener un elemento de React
+import App from "./app";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./CSS/estilos.css";
+
+// 4) Tener una funcion render corriendo
+ReactDOM.render(<App/>, document.querySelector("#root"));
